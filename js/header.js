0d12699e9acc418aaf263d818962bf8387da4176
ParadoksI <wwesuper146@gmail.com>
2024-03-02 00:32:37 +0900

    Первая версия страницы сделана

--- js/header.js
+++ js/header.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
+    var lastScrollTop = 0;
+    var delta = 5;
+    var headerHeight = $('#header').outerHeight();
+
+    $(window).scroll(function () {
+        if ($(window).width() < 710) { 
+            var isFlex = $('.header__list').css('display') === 'flex';
+            if (isFlex) { 
+
+            } else { 
+                var scroll = $(this).scrollTop();
+                if (Math.abs(lastScrollTop - scroll) <= delta)
+                    return;
+        
+                if (scroll > lastScrollTop && scroll > headerHeight) {
+                    
+                    
+                    $('#header').removeClass('header-down').addClass('header-up');
+                    $('#toggleBtn').removeClass('header-down').addClass('header-up');
+                } else {
+                   
+                    if (scroll + $(window).height() < $(document).height()) {
+                        $('#header').removeClass('header-up').addClass('header-down');
+                        $('#toggleBtn').removeClass('header-up').addClass('header-down');
+                    }
+                }
+                lastScrollTop = scroll;
+            }
+            
+        };
+        
+    });
+
     if ($(window).width() > 710) {
         $('#toggleBtn').click(function () {
             $('.wrapper').toggleClass('active');
@@ -17,6 +50,4 @@ $(document).ready(function () {
             }).css('display', 'flex');
         });
     }
-
-    
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,37 @@
-$(document).ready(function() {
-    $('.serv__card__btn').mousemove(function(event) {
+$(document).ready(function () {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.section').forEach(section => {
+        gsap.from(section, {
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power4.out",
+            scrollTrigger: {
+                trigger: section,
+                start: "top 80%",
+                toggleActions: "play none none none",
+            }
+        });
+    });
+
+    gsap.utils.toArray('.benefits__item').forEach(item => {
+        gsap.fromTo(item.querySelector('.benefits__disc'), {
+            opacity: 0,
+            y: -20
+        }, {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            scrollTrigger: {
+                trigger: item,
+                start: "top 80%", // Начать анимацию, когда верх 80% элемента появится в видимой области
+                toggleActions: "play none none none" // Опции запуска анимации
+            }
+        });
+    });
+
+    $('.serv__card__btn').mousemove(function (event) {
         var x = event.pageX - $(this).offset().left;
         var y = event.pageY - $(this).offset().top;
         var percentX = Math.floor((x / $(this).outerWidth()) * 100);
@@ -7,7 +39,7 @@ $(document).ready(function() {
         $(this).css('background-image', 'radial-gradient(circle at ' + percentX + '% ' + percentY + '%, var(--hover-blue), var(--main-blue))');
     });
 
-    $('.serv__card__btn').mouseleave(function() {
+    $('.serv__card__btn').mouseleave(function () {
         $(this).css('background-image', 'none');
     });
 
