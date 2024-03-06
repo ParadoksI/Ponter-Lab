882af0faf400d5b5a2712bcdb39e66b5f6dca4aa
ParadoksI <wwesuper146@gmail.com>
2024-03-07 03:16:53 +0900

    Небольшие правки по сайту

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    var prevArrow = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" class="prev-arrow" viewBox="0 0 16 16"> <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/> </svg>';
+    var nextArrow = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#fff" class="next-arrow" viewBox="0 0 16 16"> <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/> </svg>';
+
     $('.gallery__slider').each(function () {
         $(this).slick({
             slidesToShow: 1,
@@ -7,6 +10,8 @@ $(document).ready(function () {
             autoplaySpeed: 5000,
             dots: false,
             arrows: true,
+            prevArrow: prevArrow,
+            nextArrow: nextArrow  
         });
     });
 
@@ -42,13 +47,14 @@ $(document).ready(function () {
 
         $check.text($check.text() == '-' ? '+' : '-');
 
-        // Скрываем все описания
+
         $('.gallery__item__disc').slideUp(500);
 
-        // Сворачиваем описание текущего элемента
+
         $this.find('.gallery__item__disc').slideDown(500);
 
-        // Заменяем текст на "+", если он не у текущего элемента
         $otherItems.find('.gallery__check').text('+');
     });
+
+    
 });
--- js/slider.js
+++ js/slider.js
@@ -4,12 +4,12 @@ $(document).ready(function(){
             slidesToShow: 1,
             slidesToScroll: 1,
             autoplay: true,
-            autoplaySpeed: 5000,
-            infinite: true,
+            autoplaySpeed: 500000,
+            infinite: false,
             dots: true,
             arrows: false,
-            centerMode : false,
-            centerPadding : "30px"
+            centerMode : true,
+            centerPadding : "60px"
         });
         
     }
